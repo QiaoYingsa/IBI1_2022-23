a4d4c1071cb1e21bd559cd50c9f66221ac93238d
QiaoYingsa <127171174+QiaoYingsa@users.noreply.github.com>
2023-03-15 12:48:24 +0800

    Update hexagons.py

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,6 +1,11 @@
+# n begins from one.
 n=1
+# Loop begins and continues when n equals 1,2,3,4,5
 while n<6:
+    #use the formula to calculate the h
     h=2*n*(2*n-1)/2
+    #print the h
     print(h)
+    #move to the next n
     n=n+1
     continue
