e5e48b014eb9f2f1a53839a03a38b35dbe319806
QiaoYingsa <yingsa.22@intl.zju.edu.cn>
2023-03-15 10:04:08 +0100

    variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,10 +1,13 @@
 a=-3.19
 b=-118.24
 c=116.39
-d=a-b
-e=c-a
-d>=e #If the result is right, Rob travels further to Los Angeles. If not, Haining id further.
+d=abs(a-b)
+e=abs(c-a)
+d>=e #If the result is right, Rob travels further to Los Angeles. If not, Haining is further.
+# My answer is: Haining is further.
 X=True
 Y=False
 W=X and Y #W is false.
-Z=X or Y #Z is true.
+Z=X or Y #Z is true。
+print(W)
+print(Z)
