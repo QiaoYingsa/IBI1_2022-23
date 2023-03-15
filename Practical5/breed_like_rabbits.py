565c7bef1afd84d43d795eae5100e1c814592d05
QiaoYingsa <yingsa.22@intl.zju.edu.cn>
2023-03-15 09:13:13 +0100

    breed_like_rabbits.py

---
+++ Practical5/breed_like_rabbits.py
@@ -0,0 +1,13 @@
+i=1
+n=2
+while n<100:
+   print(n)
+   n=2*n
+   i=i+1
+if n==100:
+   i=i+1
+   print("The number of generations required to exceed 100 rabbits is " + str(i))
+else:
+   print("The number of generations required to exceed 100 rabbits is " + str(i))
+
+   
